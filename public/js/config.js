c4df87e06d9dee205bf0cec7a1baa5a590ec9c57
David Liebherr <ambition.aidev@gmail.com>
2025-06-02 21:43:55 -0500

    config fixc

--- public/js/config.js
+++ public/js/config.js
@@ -1,3 +1,5 @@
 // config.js - Configuration constants
-export const api = "http://localhost:3000/api";
+export const api = window.location.hostname === 'localhost' 
+  ? "http://localhost:3000/api"
+  : "https://staceys-blog.onrender.com/api";
 export const POSTS_PER_PAGE = 5;
